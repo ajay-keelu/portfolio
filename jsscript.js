cd9379dcdd280f3ec6ee40b3fbc8fee1be7d7bf0
AJAY KEELU <20a91a05f6@aec.edu.in>
2023-07-15 18:47:39 +0530

    adding doc

---
+++ jsscript.js
@@ -0,0 +1,13 @@
+let header = document.querySelector('#header')
+let navbarBtn = document.querySelector('.navbar-btn')
+let navbar = document.querySelector('#navbar');
+navbarBtn.addEventListener('click',()=>{
+    if(navbar.style.display === 'block'){
+        navbar.style.display = 'none';
+        header.style.height = '100px';
+    }else{
+        navbar.style.display = 'block';
+        header.style.height = '200px'
+    }
+})
+
